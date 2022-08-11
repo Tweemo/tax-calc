4d48fa2c85ffa0000e9bacabf03dbc1844621d9a
Tweemo <timothyliew88@hotmail.com>
2022-08-11 17:09:05 +1200

    initial commit

---
+++ tax_calc.py
@@ -0,0 +1,53 @@
+'''
+Program that calculates TAXes and returns the total amount of TAXes.
+'''
+
+import sys
+
+sys.tracebacklimit = 0
+
+try:
+    TAXABLE_INCOME = int(input('Enter Taxable income: '))
+except ValueError:
+    print("You must enter a number.")
+except NameError:
+    print("Do not include commas.")
+# If TAXable income entered is not a number, the program will fire an error.
+# If TAXable income contains commas, send error saying no commas allowed.
+
+# < $14000 = 10.5% TAX
+# $14,000 < && < $48,000= 17.5% TAX
+# $48,000 < && < $70,000= 30.0% TAX
+# $70,000 < && < $180,000= 33.0% TAX
+# $180,000 < = 39.0% TAX
+
+FIRST_TAX_BRACKET = 14000 * 0.105
+SECOND_TAX_BRACKET = FIRST_TAX_BRACKET + 34000 * 0.175
+THIRD_TAX_BRACKET = SECOND_TAX_BRACKET + 22000 * 0.3
+FOURTH_TAX_BRACKET = THIRD_TAX_BRACKET + 110000 * 0.33
+
+# Check if eligible for Independent earner tax credit (IETC)
+# https://www.ird.govt.nz/income-tax/income-tax-for-individuals/individual-tax-credits/independent-earner-tax-credit-ietc
+IETC = 24000 <= TAXABLE_INCOME <= 44000
+
+if TAXABLE_INCOME <= 14000:
+    TAX = TAXABLE_INCOME * 0.105
+elif 14000 < TAXABLE_INCOME <= 48000:
+    CURRENT_TAXABLE_INCOME = TAXABLE_INCOME - 14000
+    CURRENT_TAX_BRACKET = CURRENT_TAXABLE_INCOME * 0.175
+    if IETC is True:
+        TAX = FIRST_TAX_BRACKET + CURRENT_TAX_BRACKET - 520
+    else: TAX = FIRST_TAX_BRACKET + CURRENT_TAX_BRACKET
+elif 48000 < TAXABLE_INCOME <= 70000:
+    CURRENT_TAXABLE_INCOME = TAXABLE_INCOME - 48000
+    TAX = SECOND_TAX_BRACKET + CURRENT_TAXABLE_INCOME * 0.3
+elif 70000 < TAXABLE_INCOME <= 180000:
+    CURRENT_TAXABLE_INCOME = TAXABLE_INCOME - 70000
+    TAX = THIRD_TAX_BRACKET + CURRENT_TAXABLE_INCOME * 0.33
+elif TAXABLE_INCOME > 180000:
+    CURRENT_TAXABLE_INCOME = TAXABLE_INCOME - 180000
+    TAX = FOURTH_TAX_BRACKET + CURRENT_TAXABLE_INCOME * 0.39
+else:
+    print('Error')
+
+print(f'You need to pay ${round(TAX, 2)} in taxes.')
